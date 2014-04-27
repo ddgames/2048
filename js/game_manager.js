758b39674a66be914344c97678f2c98b6916eeb7
ddgames <james.meyers212@gmail.com>
2014-04-26 22:06:14 -0400

    commit

--- js/game_manager.js
+++ js/game_manager.js
@@ -158,7 +158,7 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 2048);
+          var merged = new Tile(positions.next, tile.value * 1024);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
